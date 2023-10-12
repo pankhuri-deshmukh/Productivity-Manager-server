f108a39c3d4323fd6521134850284bad39eeda51
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-10-12 23:30:45 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+import { userRouter } from './routes/users.js';
+
+dotenv.config();
+
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/auth", userRouter);
+
+//connect to MongoDB database
+mongoose.connect("mongodb+srv://pankhuri:"+process.env.DB_PASSWORD+"@manager-db.rn0vt9p.mongodb.net/manager-db?retryWrites=true&w=majority")
+
+app.listen(3001, () => console.log("Server running on port 3001..."))
