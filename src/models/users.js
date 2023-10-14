1b82e5e676a379ed4bf1af5bdd87f41c7868ebda
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-10-14 08:33:43 +0530

    update

--- src/models/users.js
+++ src/models/users.js
@@ -1,8 +1,37 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: {type:String, required:true, unique:true},
-    username: {type:String, required:true}
+    name : {
+        type:String ,
+        required:true,
+      } ,
+      email: {
+          type: String,
+          required:true,
+          unique: true,
+        },
+        password: {
+            type: String,
+          },
+          date: {
+              type: Date,
+              default: Date.now
+            },
+              active: {
+                type: Boolean,
+                required:true,
+                default: false
+                },
+                googleId: {
+                  type:String ,
+                
+                } ,
+                role: {
+                    type: String,
+                    required: true,
+                    default:"user"
+                    //roles user and admin
+                  },
 });
 
 export const UserModel = mongoose.model("users", UserSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,26 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import {UserModel} from '../models/users.js'
 
 const router = express.Router();
 
-router.post('/register', async (res, req) => {
-    
+router.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const user = await UserModel.findOne({email: email})
+    if(user) {
+        return res.json({message : "User already exists."})
+    }
+
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({email : email, password: hashedPwd, name:name})
+    await newUser.save();
+
+    res.json({message: "User registered successfully"});
 });
+
+
 
 export {router as userRouter};
