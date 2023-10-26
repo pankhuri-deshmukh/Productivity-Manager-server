eabb7c4db0e7558862b6b8bdc283eae20d488f7b
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-10-26 12:43:55 +0530

    building login

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,9 @@ import express from "express";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import {UserModel} from '../models/users.js'
+import * as dotenv from 'dotenv'
+
+dotenv.config();
 
 const router = express.Router();
 
@@ -20,6 +23,26 @@ router.post('/register', async (req, res) => {
 
     res.json({message: "User registered successfully"});
 });
+
+router.post('/login', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const user = await UserModel.findOne({email: email})
+    if(!user) {
+        return res.json({message : "User does not exist."})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if(!isPasswordValid) {
+        return res.json({message : "Username or passoword is invalid."})
+    }
+
+    //Imp note: never add personal user information in the jwt token, it becomes vulnerable to misuse
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+    res.json({token})
+    
+})
 
 
 
